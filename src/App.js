f7163cab7c283e548b5b52230267f0cbf001868d
KelliePetersen <kelliepetersen@outlook.com>
2020-06-02 20:00:00 -0600

    fetch data from the Covid API and send to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { fetchData } from './api';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 
 export class App extends Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
